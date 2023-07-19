1cbabc2accedd57f4e2ed3ddb2518ae0b8238cea
Willian Ozako <willian.ozako@gmail.com>
2023-07-18 21:34:39 -0300

    Implement handler to get the user himself

--- backend/handler/user_handler.go
+++ backend/handler/user_handler.go
@@ -19,3 +19,41 @@ func GetAllUsers(c *fiber.Ctx) error {
 	c.JSON(users)
 	return nil
 }
+
+func GetSingleUser(c *fiber.Ctx) error {
+	// TODO: get username
+	// FIXME: protect me against SQL injection
+	//        https://gorm.io/docs/security.html
+	username := c.Params("username")
+
+	var user models.UserPublic
+
+	result := database.DBConnection.Model(&models.User{}).Find(&user, username)
+
+	if result.Error != nil {
+		c.Status(500)
+		return nil
+	}
+
+	c.JSON(user)
+	return nil
+}
+
+func GetUserHimself(c *fiber.Ctx) error {
+	// TODO: get username
+	// FIXME: protect me against SQL injection
+	//        https://gorm.io/docs/security.html
+	username := "admin"
+
+	var user models.UserPrivate
+
+	result := database.DBConnection.Model(&models.User{}).Find(&user, username)
+
+	if result.Error != nil {
+		c.Status(500)
+		return nil
+	}
+
+	c.JSON(user)
+	return nil
+}
