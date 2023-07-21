48b676ad07f4b57344eeccbeaa8886ba740f73b7
Willian Ozako <willian.ozako@gmail.com>
2023-07-20 21:15:04 -0300

    Implement function to filter for private user's data

--- backend/selector/user_selector.go
+++ backend/selector/user_selector.go
@@ -8,6 +8,41 @@ type UserPublicApi struct {
 	Histories []HistoryApi
 }
 
+type UserPrivateApi struct {
+	Username  string
+	Name      string
+	Email     string
+	Role      string
+	Histories []HistoryApi
+}
+
+func GetPrivateUserData(user models.User) UserPrivateApi {
+
+	if user.Histories == nil {
+		return UserPrivateApi{
+			user.Username,
+			user.Name,
+			user.Email,
+			user.Role,
+			nil,
+		}
+	}
+
+	var histories []HistoryApi
+
+	for _, history := range user.Histories {
+		histories = append(histories, GetHistoryData(history))
+	}
+
+	return UserPrivateApi{
+		user.Username,
+		user.Name,
+		user.Email,
+		user.Role,
+		histories,
+	}
+}
+
 func GetPublicUserData(user models.User) UserPublicApi {
 
 	if user.Histories == nil {
