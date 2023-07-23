64ee3e0c159276c24b89eb6998b8c285aee8909c
Willian Ozako <willian.ozako@gmail.com>
2023-07-23 16:07:34 -0300

    Include GET users endpoints

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"github.com/Oozaku/bug-bug-tracker/backend/database"
+	"github.com/Oozaku/bug-bug-tracker/backend/router"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	database.OpenDatabaseConnection()
+
+	app := fiber.New()
+	router.SetupRoutes(app)
+
+	app.Listen(":3000")
 }
