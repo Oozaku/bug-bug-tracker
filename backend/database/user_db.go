0aec8a778535754f44026648c38da66842ad956f
Willian Ozako <willian.ozako@gmail.com>
2023-07-27 21:38:22 -0300

    Implement get User from db

---
+++ backend/database/user_db.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"errors"
+
+	"github.com/Oozaku/bug-bug-tracker/backend/database/models"
+	"github.com/Oozaku/bug-bug-tracker/backend/validator"
+	"gorm.io/gorm"
+)
+
+func getUser(username string) (models.User, error) {
+	if err := validator.ValidateUsersUsername(username); err != nil {
+		return models.User{}, err
+	}
+
+	var user models.User
+
+	err := DBConnection.Model(&models.User{}).Preload("Histories").First(
+		&user, "username = ?", username,
+	).Error
+
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return models.User{}, errors.New("User not found")
+		}
+
+		return models.User{}, errors.New("Database is down")
+	}
+
+	return user, nil
+}
